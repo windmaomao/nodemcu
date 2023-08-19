10f7d9716f45dcce399420183dd3603a298b85c4
Fang Jin <windmaomao@gmail.com>
2023-08-19 11:52:49 -0400

    support web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define Log(...) Serial.println(__VA_ARGS__)
 
@@ -8,16 +9,25 @@ int baudRate = 115200;
 const char *ssid = "Gfiber208";
 const char *password = "208walford";
 
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+void rootRoute()
+{
+  digitalWrite(ledPin, HIGH);
+  server.send(200, "text/plain", "Hello World!\r\n");
+  digitalWrite(ledPin, LOW);
+}
 
 void setup()
 {
   // setup the pullup led
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
+
   // setup serial
   Serial.begin(baudRate);
   Log("Start logging...");
+
   // setup wifi
   WiFi.begin(ssid, password);
   Serial.print("Wifi connecting");
@@ -28,8 +38,15 @@ void setup()
   }
   Log("IP address:");
   Log(WiFi.localIP());
+
+  // Setup server
+  server.on("/", rootRoute);
+  Log("Route /");
+  server.begin();
+  Log("Http server started.");
 }
 
 void loop()
 {
+  server.handleClient();
 }
