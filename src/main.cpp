ddd3a7f71757fce77a2141742b7c11ac744c3af1
Fang Jin <windmaomao@gmail.com>
2023-08-19 11:31:12 -0400

    add serial support

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 
+#define Log(...) Serial.println(__VA_ARGS__)
+
 int ledPin = LED_BUILTIN;
+int baudRate = 115200;
 
 void setup()
 {
+  // setup the pullup led
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin, LOW);
+  // setup serial
+  Serial.begin(baudRate);
+  Log("Start logging...");
 }
 
 void loop()
